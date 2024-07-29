fe59029ddb8884a610a6d264e1df50a167695956
fennys <deivy273@gmail.com>
2024-07-29 06:13:20 -0500

    ..

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 http.createServer(function(request, response) {
 response.writeHead(200, {"Content-Type": "text/html"});
-response.write("Hola Desarrollo Web");
+response.write("Hola Desarrollo Web,como estan");
 response.end();
 }).listen(8888);
